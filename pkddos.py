fb54e106961aafde13d21efa9606548b2923bb14
Kenne400k <115067774+Kenne400k@users.noreply.github.com>
2023-02-06 21:07:15 +0700

    Update pkddos.py

--- pkddos.py
+++ pkddos.py
@@ -1,11 +1,11 @@
 import os, sys,re,json,random
 from time import sleep
-import threading 
-from time import strftime
-try:
-	import requests,pystyle
-except:
-	os.system('pip install requests && pip install bs4 && pip install pystyle && pip install clear && pip install strftime')
+import threading
+import requests
+import pystyle
+import utils
+
+os.system('pip unstall requests && pip install requests && pip install bs4 && pip install pystyle && pip install clear && pip install utils')
 
 from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
 def ra(a):
@@ -14,7 +14,7 @@ def ra(a):
      sys.stdout.flush()
 from pystyle import Box
 os.system('clear')
-print('nhập số 1 để vào tool')
+print('nhấn 1 để vào tool')
 abc = int(input("Nhập Số : ")) 
 try:
   if abc == 1:
@@ -24,5 +24,4 @@ try:
 
 except:
    pass
-
 
